5a83afc704eab17e946debfa92ffa4e3cae8c5af
antarho <horacio.antar@plugtree.com>
2014-11-25 12:14:34 -0300

    cleaning a postman json

--- droolsExample-guvnor/src/main/java/com/plugtree/dslExample/guvnor/GuvnorExample.java
+++ droolsExample-guvnor/src/main/java/com/plugtree/dslExample/guvnor/GuvnorExample.java
@@ -12,54 +12,57 @@ import org.apache.cxf.jaxrs.client.WebClient;
 import javax.ws.rs.client.Entity;
 
 public final class GuvnorExample {
-  
-  public static void main(final String[] args) {
-	  addingNewPackage();
-	  addingNewAsset();
-    
-  }
-  
-  private static void addingNewPackage(){
-	  WebClient client = WebClient.create("http://localhost:8080");
 
-	  final Entity entity = Entity.xml("<?xml version=\"1.0\" encoding=\"UTF-8\"?><package><description>Hernsys Package Example Description</description><title>testPackage</title></package>");
+	public static void main(final String[] args) {
+		addingNewPackage();
+		addingNewAsset();
 
-	    Response response = client.path("guvnor/rest/packages")
-	          	.accept(MediaType.APPLICATION_JSON_TYPE)
-	          	.header("Authorization", "Basic Z3Vlc3Q6Z3Nlc2QK=")
-	          	.type(MediaType.APPLICATION_OCTET_STREAM_TYPE)
-	          	.post(entity);
+	}
 
-	 	System.out.println("Status:" + response.getStatus());
-	 	System.out.println("Headers:" + response.getHeaders());
-	  
-  }
-  
-  private static void addingNewAsset(){
-	  WebClient client = WebClient.create("http://localhost:8080");
+	@SuppressWarnings("rawtypes")
+	private static void addingNewPackage() {
+		WebClient client = WebClient.create("http://localhost:8080");
 
-	 	byte[] fileBytes = null;
+		final Entity entity = Entity
+				.xml("<?xml version=\"1.0\" encoding=\"UTF-8\"?><package><description>Hernsys Package Example Description</description><title>testPackage</title></package>");
+
+		Response response = client.path("guvnor/rest/packages")
+				.accept(MediaType.APPLICATION_JSON_TYPE)
+				.header("Authorization", "Basic Z3Vlc3Q6Z3Nlc2QK=")
+				.type(MediaType.APPLICATION_OCTET_STREAM_TYPE).post(entity);
+
+		System.out.println("Status:" + response.getStatus());
+		System.out.println("Headers:" + response.getHeaders());
+
+	}
+
+	private static void addingNewAsset() {
+		WebClient client = WebClient.create("http://localhost:8080");
+
+		byte[] fileBytes = null;
 		try {
-			fileBytes = Files.readAllBytes(Paths.get("src/main/resources/com/resources/guvnor/assets/guvnorRuleExample.drl"));
+			fileBytes = Files
+					.readAllBytes(Paths
+							.get("src/main/resources/com/resources/guvnor/assets/guvnorRuleExample.drl"));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
-	 	String fileSource = new String(fileBytes);
+		String fileSource = new String(fileBytes);
 
-	 	System.out.println(fileSource);
+		System.out.println(fileSource);
 
-	    Response response = client.path("guvnor/rest/packages/testPackage/assets")
-	          	.accept(MediaType.APPLICATION_ATOM_XML_TYPE)
-	          	.header("Authorization", "Basic Z3Vlc3Q6Z3Nlc2QK=")
-	          	.type(MediaType.APPLICATION_OCTET_STREAM_TYPE)
-	          	.header("slug", "guvnorRuleExample.drl")
-	          	.post(fileSource);
+		Response response = client
+				.path("guvnor/rest/packages/testPackage/assets")
+				.accept(MediaType.APPLICATION_ATOM_XML_TYPE)
+				.header("Authorization", "Basic Z3Vlc3Q6Z3Nlc2QK=")
+				.type(MediaType.APPLICATION_OCTET_STREAM_TYPE)
+				.header("slug", "guvnorRuleExample.drl").post(fileSource);
 
-	 	System.out.println("Status:" + response.getStatus());
-	 	System.out.println("Headers:" + response.getHeaders());
-	  
-  }
-  
+		System.out.println("Status:" + response.getStatus());
+		System.out.println("Headers:" + response.getHeaders());
+
+	}
+
 }
